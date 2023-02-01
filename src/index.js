4ec1a9d9b798346a5aad24b9b677715dc6662e63
Maria <>
2023-02-01 22:35:14 +0800

    add agina some comment

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-//this is comment 
+//this is comment this is comment
 root.render(
   <React.StrictMode>
     <App />
